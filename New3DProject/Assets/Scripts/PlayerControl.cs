95405b83b0c10a79fc160afcd3cd2b1b90125429
Alpka <91074812+Alpka@users.noreply.github.com>
2021-09-26 02:19:41 +0400

    some shit

--- New3DProject/Assets/Scripts/PlayerControl.cs
+++ New3DProject/Assets/Scripts/PlayerControl.cs
@@ -15,28 +15,27 @@ public class PlayerControl : MonoBehaviour
         if (thisObject != null)
         {
             //  Work, but bad!
-            /*int layerMask = 1 << 8;
+            int layerMask = 1 << 8;
             layerMask = ~layerMask;
+            float objectScale = thisObject.transform.lossyScale.x;
+            float lowRay = 0.24f;
             RaycastHit hit1, hit2;
-            ;
-            Physics.Raycast(thisObject.transform.position, Vector3.back, out hit2, 0.25f);
             if (Input.GetAxis("Vertical") > 0 &&
-                (!Physics.Raycast(thisObject.transform.position + new Vector3(0.25f, -0.15f, 0), Vector3.forward, out hit1, 0.26f, layerMask) &&
-                !Physics.Raycast(thisObject.transform.position - new Vector3(0.25f, 0.15f, 0), Vector3.forward, out hit2, 0.26f, layerMask)))
-                thisObject.transform.position += new Vector3(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * objectSpeed);
+                (!Physics.Raycast(thisObject.transform.position + new Vector3(lowRay, - 0.15f, 0) * objectScale, Vector3.forward, out hit1, 0.26f * objectScale, layerMask) &&
+                !Physics.Raycast(thisObject.transform.position - new Vector3(lowRay, 0.15f, 0) * objectScale, Vector3.forward, out hit2, 0.26f * objectScale, layerMask)))
+                thisObject.transform.position += new Vector3(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * objectSpeed * objectScale);
             if (Input.GetAxis("Vertical") < 0 &&
-                (!Physics.Raycast(thisObject.transform.position + new Vector3(0.25f, -0.15f, 0), Vector3.back, out hit1, 0.26f, layerMask) &&
-                !Physics.Raycast(thisObject.transform.position - new Vector3(0.25f, 0.15f, 0), Vector3.back, out hit2, 0.26f, layerMask)))
-                thisObject.transform.position += new Vector3(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * objectSpeed);
+                (!Physics.Raycast(thisObject.transform.position + new Vector3(lowRay, -0.15f, 0) * objectScale, Vector3.back, out hit1, 0.26f * objectScale, layerMask) &&
+                !Physics.Raycast(thisObject.transform.position - new Vector3(lowRay, 0.15f, 0) * objectScale, Vector3.back, out hit2, 0.26f * objectScale, layerMask)))
+                thisObject.transform.position += new Vector3(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * objectSpeed * objectScale);
             if (Input.GetAxis("Horizontal") > 0 &&
-                (!Physics.Raycast(thisObject.transform.position + new Vector3(0, -0.15f, 0.25f), Vector3.right, out hit1, 0.26f, layerMask) &&
-                !Physics.Raycast(thisObject.transform.position - new Vector3(0, 0.15f, 0.25f), Vector3.right, out hit2, 0.26f, layerMask)))
-                thisObject.transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * objectSpeed, 0, 0);
+                (!Physics.Raycast(thisObject.transform.position + new Vector3(0, -0.15f, lowRay) * objectScale, Vector3.right, out hit1, 0.26f * objectScale, layerMask) &&
+                !Physics.Raycast(thisObject.transform.position - new Vector3(0, 0.15f, lowRay) * objectScale, Vector3.right, out hit2, 0.26f * objectScale, layerMask)))
+                thisObject.transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * objectSpeed * objectScale, 0, 0);
             if (Input.GetAxis("Horizontal") < 0 &&
-                (!Physics.Raycast(thisObject.transform.position + new Vector3(0, -0.15f, 0.25f), Vector3.left, out hit1, 0.26f, layerMask) &&
-                !Physics.Raycast(thisObject.transform.position - new Vector3(0, 0.15f, 0.25f), Vector3.left, out hit2, 0.26f, layerMask)))
-                thisObject.transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * objectSpeed, 0, 0);*/
-            thisObject.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * objectSpeed * Time.deltaTime;
+                (!Physics.Raycast(thisObject.transform.position + new Vector3(0, -0.15f, lowRay) * objectScale, Vector3.left, out hit1, 0.26f * objectScale, layerMask) &&
+                !Physics.Raycast(thisObject.transform.position - new Vector3(0, 0.15f, lowRay) * objectScale, Vector3.left, out hit2, 0.26f * objectScale, layerMask)))
+                thisObject.transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * objectSpeed * objectScale, 0, 0);
         }
     }
 }
