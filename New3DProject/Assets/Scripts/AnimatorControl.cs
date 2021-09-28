145ad00d599813d01524410f7667d3dc4298a6be
Alpka <91074812+Alpka@users.noreply.github.com>
2021-09-28 13:14:41 +0400

    animation

--- New3DProject/Assets/Scripts/AnimatorControl.cs
+++ New3DProject/Assets/Scripts/AnimatorControl.cs
@@ -4,65 +4,15 @@ using UnityEngine;
 
 public class AnimatorControl : MonoBehaviour
 {
-    private int dir;
-    private bool isMoving;
-    private float lastPosX, lastPosZ;
-    private void Awake()
-    {
-        PositionRefresh();
-    }
+    public int dir;
+    public bool isMoving;
     private void Update()
     {
-        isMoving = MoveCheck();
-        DirCheck();
-        ControlAnimation();
-        PositionRefresh();
+        
     }
     private void ControlAnimation()
     {
         gameObject.GetComponent<Animator>().SetInteger("dir", dir);
         gameObject.GetComponent<Animator>().SetBool("isMoving", isMoving);
-    }
-    private void DirCheck() 
-    {
-        if (isMoving)
-        {
-            if (lastPosX != gameObject.transform.position.x)
-            {
-                if (Mathf.Abs(lastPosX - gameObject.transform.position.x) > 0.025f) 
-                {
-                    if (gameObject.transform.position.x > lastPosX) // move right
-                        dir = 3;
-                    else                                            // move left
-                        dir = 1; 
-                }
-            }
-            else
-            {
-                if (Mathf.Abs(lastPosZ - gameObject.transform.position.z) > 0.025f)
-                {
-                    if (gameObject.transform.position.z > lastPosZ) // move up
-                        dir = 2;
-                    else                                            // move down
-                        dir = 0;
-                }
-            }
-        }
-    }
-    private void PositionRefresh() //must be called on end of script
-    {
-        lastPosX = gameObject.transform.position.x;
-        lastPosZ = gameObject.transform.position.z;
-    }
-
-    private bool MoveCheck()
-    {
-        bool m_isMoving;
-        if (lastPosX != gameObject.transform.position.x ||
-            lastPosZ != gameObject.transform.position.z)
-            m_isMoving = true;
-        else
-            m_isMoving = false;
-        return m_isMoving;
     }
 }
--- New3DProject/Assets/Scripts/PlayerControl.cs
+++ New3DProject/Assets/Scripts/PlayerControl.cs
@@ -15,15 +15,34 @@ public class PlayerControl : MonoBehaviour
         if (thisObject != null)
         {
             CollChek blockChek = thisObject.GetComponent<CollChek>();
+            AnimatorControl anim = thisObject.GetComponent<AnimatorControl>();
+            bool isMoving = false;
             float objectScale = thisObject.transform.lossyScale.x;
             if (Input.GetAxis("Vertical") > 0 && !blockChek.IsForwardBlocked())
+            {
                 thisObject.transform.position += new Vector3(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * objectSpeed * objectScale);
+                anim.dir = 2;
+                isMoving = true;
+            }
             if (Input.GetAxis("Vertical") < 0 && !blockChek.IsBackBlocked())
+            {
                 thisObject.transform.position += new Vector3(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * objectSpeed * objectScale);
+                anim.dir = 0;
+                isMoving = true;
+            }
             if (Input.GetAxis("Horizontal") > 0 && !blockChek.IsLeftBlocked())
+            {
                 thisObject.transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * objectSpeed * objectScale, 0, 0);
+                anim.dir = 3;
+                isMoving = true;
+            }
             if (Input.GetAxis("Horizontal") < 0 && !blockChek.IsRightBlocked())
+            {
                 thisObject.transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * objectSpeed * objectScale, 0, 0);
+                anim.dir = 1;
+                isMoving = true;
+            }
+            anim.isMoving = isMoving;
         }
     }
 }
