8dc39f74a6bcdc7838705d559f4eb75459be8251
Alpka <91074812+Alpka@users.noreply.github.com>
2021-10-10 23:27:04 +0400

    dude don't fuck wall

--- New3DProject/Assets/Scripts/Player stuff/PlayerControl.cs
+++ New3DProject/Assets/Scripts/Player stuff/PlayerControl.cs
@@ -14,73 +14,68 @@ public class PlayerControl : MonoBehaviour
     {
         if (thisObject != null)
         {
-            GameObject armature = GameObject.Find("/" + thisObject.name + "/Armature");
+            GameObject armature = thisObject.transform.Find("Armature").gameObject;
+            CollChek collChek = thisObject.GetComponent("CollChek") as CollChek;
             float objectScale = thisObject.transform.lossyScale.x;
             Animator anim = armature.GetComponent<Animator>();
-            anim.SetBool("isMoving", MoveToDir(objectScale * objectSpeed * Time.deltaTime, thisObject));
+            anim.SetBool("isMoving", MoveToDir(objectScale * objectSpeed * Time.deltaTime, thisObject, armature, collChek));
         }
     }
-    private bool MoveToDir(float m_objectSpeed, GameObject m_thisObject)
+    private bool MoveToDir(float m_objectSpeed, GameObject m_thisObject, GameObject m_armature, CollChek m_collChek)
     {
+        
         float rotSpeed = 5;
         if ((Input.GetAxis("Horizontal") != 0) || (Input.GetAxis("Vertical") != 0))
         {
-            if (Input.GetAxis("Vertical") > 0)
+            if (Input.GetAxis("Vertical") > 0 && !m_collChek.IsForwardBlocked())
             {
-                if (Input.GetAxis("Horizontal") > 0)
+                if (Input.GetAxis("Horizontal") > 0 && !m_collChek.IsRightBlocked())
                 {
                     m_thisObject.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * m_objectSpeed;
-                    m_thisObject.transform.rotation = Quaternion.Slerp(m_thisObject.transform.rotation, Quaternion.Euler(0, 45, 0), rotSpeed * Time.deltaTime);
-                    //m_thisObject.transform.rotation = Quaternion.Euler(RotateTo(45, m_thisObject));
+                    m_armature.transform.rotation = Quaternion.Slerp(m_armature.transform.rotation, Quaternion.Euler(-90, 45, 0), rotSpeed * Time.deltaTime);
                     return true;
                 }
-                if (Input.GetAxis("Horizontal") < 0)
+                if (Input.GetAxis("Horizontal") < 0 && !m_collChek.IsLeftBlocked())
                 {
                     m_thisObject.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * m_objectSpeed;
-                    m_thisObject.transform.rotation = Quaternion.Slerp(m_thisObject.transform.rotation, Quaternion.Euler(0, 315, 0), rotSpeed * Time.deltaTime);
-                    //m_thisObject.transform.rotation = Quaternion.Euler(RotateTo(315, m_thisObject));
-                    return true;
-                }
-                m_thisObject.transform.position += new Vector3(0, 0, Input.GetAxis("Vertical")) * m_objectSpeed;
-                m_thisObject.transform.rotation = Quaternion.Slerp(m_thisObject.transform.rotation, Quaternion.Euler(0, 0, 0), rotSpeed * Time.deltaTime);
-                //m_thisObject.transform.rotation = Quaternion.Euler(RotateTo(360, m_thisObject));
-                return true;
-            }
-            if (Input.GetAxis("Vertical") < 0)
-            {
-                if (Input.GetAxis("Horizontal") > 0)
-                {
-                    m_thisObject.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * m_objectSpeed;
-                    m_thisObject.transform.rotation = Quaternion.Slerp(m_thisObject.transform.rotation, Quaternion.Euler(0, 135, 0), rotSpeed * Time.deltaTime);
-                    //m_thisObject.transform.rotation = Quaternion.Euler(RotateTo(135, m_thisObject));
-                    return true;
-                }
-                if (Input.GetAxis("Horizontal") < 0)
-                {
-                    m_thisObject.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * m_objectSpeed;
-                    m_thisObject.transform.rotation = Quaternion.Slerp(m_thisObject.transform.rotation, Quaternion.Euler(0, 225, 0), rotSpeed * Time.deltaTime);
-                    //m_thisObject.transform.rotation = Quaternion.Euler(RotateTo(225, m_thisObject));
+                    m_armature.transform.rotation = Quaternion.Slerp(m_armature.transform.rotation, Quaternion.Euler(-90, 315, 0), rotSpeed * Time.deltaTime);
                     return true;
                 }
                 m_thisObject.transform.position += new Vector3(0, 0, Input.GetAxis("Vertical")) * m_objectSpeed;
-                m_thisObject.transform.rotation = Quaternion.Slerp(m_thisObject.transform.rotation, Quaternion.Euler(0, 180, 0), rotSpeed * Time.deltaTime);
-                //m_thisObject.transform.rotation = Quaternion.Euler(RotateTo(180, m_thisObject));
+                m_armature.transform.rotation = Quaternion.Slerp(m_armature.transform.rotation, Quaternion.Euler(-90, 0, 0), rotSpeed * Time.deltaTime);
                 return true;
             }
-            if (Input.GetAxis("Horizontal") > 0)
+            if (Input.GetAxis("Vertical") < 0 && !m_collChek.IsBackBlocked())
+            {
+                if (Input.GetAxis("Horizontal") > 0 && !m_collChek.IsRightBlocked())
+                {
+                    m_thisObject.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * m_objectSpeed;
+                    m_armature.transform.rotation = Quaternion.Slerp(m_armature.transform.rotation, Quaternion.Euler(-90, 135, 0), rotSpeed * Time.deltaTime);
+                    return true;
+                }
+                if (Input.GetAxis("Horizontal") < 0 && !m_collChek.IsLeftBlocked())
+                {
+                    m_thisObject.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * m_objectSpeed;
+                    m_armature.transform.rotation = Quaternion.Slerp(m_armature.transform.rotation, Quaternion.Euler(-90, 225, 0), rotSpeed * Time.deltaTime);
+                    return true;
+                }
+                m_thisObject.transform.position += new Vector3(0, 0, Input.GetAxis("Vertical")) * m_objectSpeed;
+                m_armature.transform.rotation = Quaternion.Slerp(m_armature.transform.rotation, Quaternion.Euler(-90, 180, 0), rotSpeed * Time.deltaTime);
+                return true;
+            }
+            if (Input.GetAxis("Horizontal") > 0 && !m_collChek.IsRightBlocked())
             {
                 m_thisObject.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0) * m_objectSpeed;
-                m_thisObject.transform.rotation = Quaternion.Slerp(m_thisObject.transform.rotation, Quaternion.Euler(0, 90, 0), rotSpeed * Time.deltaTime);
-                //m_thisObject.transform.rotation = Quaternion.Euler(RotateTo(90, m_thisObject));
+                m_armature.transform.rotation = Quaternion.Slerp(m_armature.transform.rotation, Quaternion.Euler(-90, 90, 0), rotSpeed * Time.deltaTime);
                 return true;
             }
-            else
+            if (Input.GetAxis("Horizontal") < 0 && !m_collChek.IsLeftBlocked())
             {
                 m_thisObject.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0) * m_objectSpeed;
-                m_thisObject.transform.rotation = Quaternion.Slerp(m_thisObject.transform.rotation, Quaternion.Euler(0, 270, 0), rotSpeed * Time.deltaTime);
-                //m_thisObject.transform.rotation = Quaternion.Euler(RotateTo(270, m_thisObject));
+                m_armature.transform.rotation = Quaternion.Slerp(m_armature.transform.rotation, Quaternion.Euler(-90, 270, 0), rotSpeed * Time.deltaTime);
                 return true;
             }
+            return false;
         }
         else
             return false;
